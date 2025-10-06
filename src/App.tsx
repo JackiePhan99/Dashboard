5e65999e4917454e7cd989cf02bb415cff71bd68
Jackie Phan <lazmonbiro@gmail.com>
2025-10-06 16:30:57 +0700

    -all

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Calendar } from './components/Calendar/Calendar';
+import { Card } from './components/Card/Card';
+
+function App() {
+  return (
+    <>
+      <Calendar />
+      <Card />
+    </>
+  )
+}
+
+export default App
